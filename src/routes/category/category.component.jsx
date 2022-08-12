1c84e99610da4d1c34bbdede6ab17674439573c6
Branden <mrbranden@gmail.com>
2022-08-11 20:38:11 -0400

    sync

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -15,6 +15,7 @@ const Category = () => {
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
+    console.log('effect fired calling setProducts');
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,4 +1,5 @@
 export const selectCategoriesMap = (state) => {
+  console.log('selector fired');
   const categoriesMap = state.categories.categories.reduce(
     (acc, { title, items }) => {
       acc[title.toLowerCase()] = items;
