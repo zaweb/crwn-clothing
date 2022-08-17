8c4d4a7b765b69e13cd9ef19a38a622f445ecd73
Branden <mrbranden@gmail.com>
2022-08-17 16:08:05 -0400

    Setting up for payments

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import {
   CheckoutContainer,
@@ -8,9 +9,6 @@ import {
   HeaderBlock,
   Total,
 } from './checkout.styles';
-
-
-import PaymentForm from '../../components/payment-form/payment-form.component';
 
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
